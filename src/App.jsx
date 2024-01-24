6fa4d77c0078c9c760b1b2ace2cd2582697d5e66
Vitor <vitorzaffari01@gmail.com>
2024-01-23 21:19:39 -0300

    refact: removed old code

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,17 @@
 
-import { useState } from 'react'
 import './App.css'
-import Player from './components/Player'
-import GameBoard from './components/GameBoard'
-import Log from './components/Log';
-import GameOver from './components/GameOver.jsx';
-import getActivePlayer from './utils/getActivePlayer.js';
-import getWinner from './utils/getWinner.js'
-import getGameBoard from './utils/getBoardGame.js';
 import MainGameWindow from './v2-components/MainGameWindow/MainGameWindow.jsx';
 
 
 
 
 function App() {
-  // const [updateBoard, setUpdateBoard] = useState([])
-
-
-  // const playerNames = {
-  //   'X': 'Player 1',
-  //   'O': 'Player 2'
-  // }
-
-  // const game = getGameBoard(updateBoard)
-
-  // let activePlayer = getActivePlayer(updateBoard)
-
-  // let winner = getWinner(playerNames, game)
-
-  // if (winner) activePlayer = null;
-
-
-  // function handleChangePlayer(row, col) {
-  //   setUpdateBoard(prev => {
-  //     let curPlayer = getActivePlayer(prev);
-  //     const newBoardGame = [{ square: { row, col }, player: curPlayer }, ...prev];
-  //     return newBoardGame;
-  //   })
-
-  // }
-  // function handleRestart() {
-  //   setUpdateBoard([]);
-  // }
-  // const draw = updateBoard.length === 9 && !winner;
-
+ 
 
   return (
     <main>
       <h1 className="app-title">LET'S PLAY <br></br> TIC-TAC-TOE</h1>
       <MainGameWindow />
-      {/* <h1 className='title'>Tic-tac-toe</h1>
-      <div className="game-box">
-        {(winner || draw) && <GameOver winner={winner} handleRestart={handleRestart} />}
-        <div className="players-box">
-          <Player name={'Player 1'} symbol={'X'} isActive={activePlayer === 'X'} />
-          <h1>|</h1>
-          <Player name={'Player 2'} symbol={'O'} isActive={activePlayer === 'O'} />
-        </div>
-        <GameBoard handleChangePlayer={handleChangePlayer} updateBoard={game} />
-
-      </div>
-      <Log updateBoard={updateBoard} winner={winner} playerNames={playerNames} /> */}
     </main>
   )
 }
--- src/utils/getActivePlayer.js
+++
@@ -1,7 +0,0 @@
-export default function getActivePlayer(gameBoard) {
-  let activePlayer = "X";
-  if (gameBoard.length > 0 && gameBoard[0].player === "X") {
-    activePlayer = "O";
-  }
-  return activePlayer;
-}
