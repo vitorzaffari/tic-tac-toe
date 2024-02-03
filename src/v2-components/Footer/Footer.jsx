6e5262b2f34804f158de6fa28f24b4998c1e3ea4
Vitor <vitorzaffari01@gmail.com>
2024-02-03 19:36:04 -0300

    update: gave better description to links

--- src/v2-components/Footer/Footer.jsx
+++ src/v2-components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <p>This website was fully developed by me, Vitor Zaffari. It's nothing new or complex, but I had fun implementing the features, including the robot (which I didn't intend to do in the first place). So, I consider this project a nice challenge for a new developer like myself. Cheers!</p>
-      <p>You can view the source code in the GitHub repo <a href='https://github.com/vitorzaffari/tic-tac-toe' target='_blank'>here</a></p>
+      <p>You can view the source code in the <a href='https://github.com/vitorzaffari/tic-tac-toe' target='_blank'>GitHub repo</a></p>
     </footer>
   )
 }
--- src/v2-components/IntroText/IntroText.jsx
+++ src/v2-components/IntroText/IntroText.jsx
@@ -8,7 +8,8 @@ const IntroText = () => {
             <div className="tictactoe-description">
                 <p>
                     Tic-tac-toe, also known as noughts and crosses or Xs and Os, is a  classic two-player game played on a 3x3 grid. The objective of the game  is to be the first player to form a line of three of their symbols (either X or O) horizontally, vertically, or diagonally on the grid.
-                    Learn more <a className='wiki-link' href="https://en.wikipedia.org/wiki/Tic-tac-toe" target='_blank'>here</a>.
+                    <br></br>
+                    Learn more on the <a className='wiki-link' href="https://en.wikipedia.org/wiki/Tic-tac-toe" target='_blank'>tic-tac-toe wiki</a>.
                 </p>
                 <p>
                 </p>
