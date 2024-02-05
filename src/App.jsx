2217a12e0e96c66168a4e7c91d998fa3bffa5711
Vitor <vitorzaffari01@gmail.com>
2024-02-05 20:03:21 -0300

    add: meta tags

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import MainGameWindow from './v2-components/MainGameWindow/MainGameWindow.jsx';
 
 function App() {
   
+
+
+  //TODO Add logs with more information to logs display
+  //TODO Fix layout for cellphones (almost there)
+  //TODO Add player and robot images on game display
+
   return (
     <main>
       <MainGameWindow />
